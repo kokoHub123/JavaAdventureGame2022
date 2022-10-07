a82dffd9b82321a58c8578ff70ea1473c981e9da
Justin Riley <20hz20khz@gmail.com>
2022-10-07 08:09:31 -0400

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -13,15 +13,42 @@ public class Enemy extends Npc
 {
   // class variables
   private static int enemyCounter = 0;
-  
+
   // class methods
   public static int getEnemyCounter()
   {
-    return enemyCounter;
+    return 0;
   }
-  
+
   // instance variables
-  // constructors
-  // methods
-  
+  // private instance vars for health, int magicWeakness, attackName
+
+  public Enemy(){
+
+  }
+
+  public Enemy(String enemyName, String enemyDescription){
+
+  }
+
+  public int getHealth(){
+    return 0;
+  }
+
+  public void loseHealth(int healthLost){
+
+  }
+
+  public String getAttackName(){
+    return "x";
+  }
+
+  public int getMagicWeakness(){
+    return 0;
+  }
+
+  public void setAttackName(String _attackName){
+
+  }
 }
+
